c43eabc4346a91677a9a0747b13615ddf83d381d
Choi-jujuyeon <chlwndus1216@naver.com>
2023-04-11 00:51:39 +0900

    특정 item의 값이 변경시에만 useEffect 실행

--- React Hooks에 취한다/useEffect마스터/ex-1/src/App.js
+++ React Hooks에 취한다/useEffect마스터/ex-1/src/App.js
@@ -8,15 +8,31 @@ function App() {
         setCount(count + 1);
     };
 
+    //마운트 될 때
     useEffect(() => {
         console.log("렌더링");
     });
     // useEffect는 인자로 콜백을 받는다.
     // 콜백 안의 코드는 매번 rendering 될 때마다 콜백을 받을 것이다.\
 
+    //마운팅 + [ item ]변경될때만 실행
+    useEffect(() => {
+        console.log("💚name 변화");
+    }, [name]);
+
+    useEffect(() => {
+        console.log("🔥count 변화");
+    }, [count]);
+
     const handleInputChange = (e) => {
         setName(e.target.value);
     };
+
+    //마운트 될 때만 실행되는 코드!!!
+    useEffect(() => {
+        console.log("처음에만 실행 ~ 마운팅때에만 ~");
+    }, []);
+
     return (
         <div>
             <button onClick={onClickChange}>Update</button>
