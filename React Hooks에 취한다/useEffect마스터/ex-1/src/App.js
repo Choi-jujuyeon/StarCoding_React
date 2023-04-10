0a585aa0d9727ab2010f494312f61574fcd45a8a
Choi-jujuyeon <chlwndus1216@naver.com>
2023-04-11 00:39:10 +0900

    매번 렌더링 될 때마다 useEffect 실행됌

--- React Hooks에 취한다/useEffect마스터/ex-1/src/App.js
+++ React Hooks에 취한다/useEffect마스터/ex-1/src/App.js
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
     const [count, setCount] = useState(1);
+    const [name, setName] = useState("");
 
     const onClickChange = () => {
         setCount(count + 1);
+    };
+
+    useEffect(() => {
+        console.log("렌더링");
+    });
+    // useEffect는 인자로 콜백을 받는다.
+    // 콜백 안의 코드는 매번 rendering 될 때마다 콜백을 받을 것이다.\
+
+    const handleInputChange = (e) => {
+        setName(e.target.value);
     };
     return (
         <div>
             <button onClick={onClickChange}>Update</button>
             <span>count: {count}</span>
+            <input type="text" value={name} onChange={handleInputChange} />
+            <span>name: {name}</span>
         </div>
     );
 }
